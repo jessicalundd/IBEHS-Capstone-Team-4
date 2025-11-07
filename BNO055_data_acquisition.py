7c05890e5ed5dde1bb591450f3526729c4e78191
Jessica <jessicalundd@yahoo.com>
2025-11-07 13:56:39 -0500

    Nov 7 - Calculate home angle and acceleration for calibration - continue displaying measurements after calibration

--- BNO055_data_acquisition.py
+++ BNO055_data_acquisition.py
@@ -1,14 +1,40 @@
 import serial
 import time
+import numpy as np
 
-initial_position = [0,0,0]
+sampling_rate = 10
+calibration_length = 3  # seconds
+
+initial_angle = []
+initial_acc = []
 
 with serial.Serial('COM3', 9600, timeout=1) as ser:
+    status = input("Ready to calibrate? Enter Y or N: ")
+
     while True:
         try:
-            line = ser.readline()
-            print(line)
-            time.sleep(0.1)
+            if status == 'Y':
+                i = 0
+                while i < calibration_length * sampling_rate * 2:
+                    current_data = str(ser.readline())
+                    if "Xe" in current_data or "Xa" in current_data:
+                        split_data = current_data.split(" ")
+                        data_array = np.asarray([float(split_data[1]), float(split_data[3]), float(split_data[5].strip("\\n'"))])
+                        if "e" in current_data:
+                            initial_angle.append(data_array)
+                        else:
+                            initial_acc.append(data_array)
+                        i += 1
+
+                    time.sleep(1/sampling_rate)
+                home_angle = np.asarray(initial_angle).mean(axis=0)
+                home_acc = np.asarray(initial_acc).mean(axis=0)
+                print(len(initial_angle))
+                status = 'N'
+
+            current_data = str(ser.readline())
+            print(current_data)
+            time.sleep(1 / sampling_rate)
         except KeyboardInterrupt:
             break
 ser.close()
