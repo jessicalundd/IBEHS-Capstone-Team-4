c0f70caa3b57c6a2c841703c4f10ec17b717ddf7
yooseonjg <yooseon.jg@gmail.com>
2026-01-20 14:38:03 -0500

    Jan 20, 2026 - Updated GUI

--- Web App/js/script.js
+++ Web App/js/script.js
@@ -1,11 +1,13 @@
 import * as THREE from 'three';
 
+const main = document.getElementById('main');
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+main.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
