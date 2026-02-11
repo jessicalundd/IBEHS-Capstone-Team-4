131dd5ac05de1fb6896dbee349dc9807b31cdba3
lamcl <laurenmclennan13@gmail.com>
2026-02-11 15:57:57 -0500

    Feb 11 - created script to verify angle accuracy of BNO055

---
+++ BNO055_Verification_Acquisition.py
@@ -0,0 +1,69 @@
+import serial
+import time
+import numpy as np
+import openpyxl
+from scipy import integrate
+
+sampling_rate = 100  # max frequency for ToF
+window_size = 10
+calibration_length = 3  # seconds
+sample_delay = 10  # how many samples to skip before updating display
+
+file_path = r"C:\Users\lamcl\Downloads\ToF Verification.xlsx"
+workbook = openpyxl.load_workbook(filename=file_path)
+sheet = workbook.active
+
+meas_time = 5  # seconds
+angle = 5  # degree
+axis = 'X'  # based on axis of rotation on sensor
+
+idx = [0 if axis == 'X' else 1 if axis == 'Y' else 2]
+meas_angles = []
+
+with serial.Serial('COM3', 9600, timeout=1) as ser:
+    def clear_serial_buffer(s):
+        time.sleep(0.5)  # wait for first junk data to arrive
+        s.reset_input_buffer()  # clears everything received but unread
+        s.reset_output_buffer()
+
+    clear_serial_buffer(ser)
+
+    # Rolling average - finding the space
+    rolling_buffer = []
+    sample_count = 0
+
+    start_time = time.time()
+    while True:
+        try:
+            if (time.time() - start_time) <= meas_time:
+                current_data = str(ser.readline())
+                print(current_data)
+                if "Xe" in current_data:
+                    split_data = current_data.split(" ")
+                    split_data[-1] = split_data[-1].strip("\\r\\n'")
+                    data_array = np.asarray(split_data[1::2], dtype=float)
+
+                current_data = data_array[idx]
+                rolling_buffer.append(current_data)
+                if len(rolling_buffer) > window_size:
+                    rolling_buffer.pop(0)
+
+                sample_count += 1
+                if len(rolling_buffer) == window_size and (sample_count % sample_delay == 0):
+                    avg = np.mean(rolling_buffer, axis=0)
+                    meas_angles.append(avg)
+                    print(f"Rolling avg: {np.round(avg, 3)}")
+
+            else:
+                new_data = [axis, angle] + meas_angles
+                sheet.append(new_data)  # adding new row of data
+                workbook.save(filename=file_path)
+                print(f"Data appended to {file_path}")
+                break
+
+        except KeyboardInterrupt:
+            break
+
+ser.close()
+
+
